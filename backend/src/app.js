3a2c5f253109712204375a1035ac61976741ec79
Ubuntu <ubuntu@ip-172-31-17-87.ap-southeast-1.compute.internal>
2025-11-09 16:38:42 +0000

    modify for ec2

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,22 +1,34 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import path from "path";
+import { fileURLToPath } from "url";
 import authRoutes from "./routes/authRoutes.js";
 import practiceRoutes from "./routes/practiceRoutes.js";
 import authMiddleware from "./middleware/authMiddleware.js";
 
 dotenv.config();
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173" }));
+// Middlewares
+app.use(cors({ origin: "https://codebattlearena.id.vn" })); // allow frontend requests
 app.use(express.json());
 
+// API Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/practice", authMiddleware, practiceRoutes);
 
+// Serve frontend (static build) - only after API routes
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 
+// Start server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`🚀 Server running at https://localhost:${PORT}`);
+  console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
